71f32fcbe5519583fd7ec3990e7e6635165dd5c6
MaherabHossain <maherabhossain10@gmail.com>
2021-04-27 14:30:34 +0600

    linked list

--- Data structure/linked_list.cpp
+++ Data structure/linked_list.cpp
@@ -10,6 +10,21 @@ struct node{
 
 struct node* head;
 
+
+
+void print(){
+
+    struct node* temp;
+
+    temp = head;
+
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+
+}
 void push(int val){
 
     struct node* temp = (struct node*) malloc(sizeof(struct node));
@@ -26,18 +41,62 @@ void push(int val){
         }
         temp2->next = temp;
     }
+
 }
 
-void print(){
+int count_number(){
 
     struct node* temp;
 
     temp = head;
-
+    int count = 0;
     while(temp != NULL){
-        cout<<temp->data<<" ";
+        count++;
         temp = temp->next;
     }
+    return count;
+}
+
+void insert_first(){
+    int n;
+    cout<<"Enter value: ";
+    cin>>n;
+    struct node* temp;
+
+    temp->data = n;
+    temp->next = head;
+    head = temp;
+    print();
+}
+
+void insert_any_possition (int pos){
+    int n;
+    cout<<"Enter value: ";
+    cin>>n;
+
+    struct node* value = (struct node*) malloc(sizeof(struct node));
+
+    value->data = n;
+    struct node* temp;
+
+    int count = 0;
+    struct node* first_part = (struct node*) malloc(sizeof(struct node));
+    first_part = head;
+    temp = first_part->next;
+    while( true ){
+        count++;
+        if(count==pos-1){
+            break;
+        }
+        first_part = temp;
+
+        temp = temp->next;
+
+    }
+    first_part->next = value;
+    value->next = temp;
+    print();
+
 
 }
 
@@ -45,13 +104,33 @@ int main()
 {
   head = NULL;
   int val;
-  cout<<"Enter first number: ";
+  cout<<"Enter first number (enter -1 for exit): ";
   cin>>val;
   while(val>0){;
         push(val);
-        cout<<"Enter next number: ";
+        cout<<"Enter next number (enter -1 for exit): ";
         cin>>val;
   }
   print();
+  while(true){
+    cout<<"Which possition you want to insert: ";
+    int pos;
+    cin>>pos;
+    if(pos<0){
+        break;
+    }
+    if(pos==1){
+        insert_first();
+
+    }else if(pos==count_number()+1){
+        cin>>val;
+        push(val);
+        print();
+    }else{
+        insert_any_possition(pos);
+    }
+  }
   return 0;
 }
+
+
